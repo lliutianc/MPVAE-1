c3bf02def893d5e2a069d085acbecb4140874972
lliutianc <lliutianc@gmail.com>
2021-10-06 02:39:08 -0400

    debug

--- model.py
+++ model.py
@@ -157,9 +157,9 @@ def compute_loss(input_label, fe_out, fe_mu, fe_logvar, fx_out, fx_mu, fx_logvar
     # the probabilities w.r.t. every label in each sample from the batch
     # size: n_sample * n_batch * label_dim
     # eps1: to ensure the probability is non-zero
-    print(B)
-    print(fe_out)
+
     print(sample_r)
+    print(sample_r.min(), sample_r.max())
     E = norm.cdf(sample_r) * (1-eps1)+eps1*0.5
     # similar for the feature branch
     E_x = norm.cdf(sample_r_x)*(1-eps1)+eps1*0.5
