9fbd49c6569b85ee3eb0bfe2ab586b47e1d497b3
lliutianc <lliutianc@gmail.com>
2021-10-07 00:51:42 -0400

    debug

--- fairmodel.py
+++ fairmodel.py
@@ -170,7 +170,7 @@ def KLdivergence(x, y):
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    critic = FairCritic(4, 256, 1)
+    critic = FairCritic(2, 256, 1)
     critic = critic.to(device)
     divergence = 'KLD'
     activation_f = Activation_f(divergence)
