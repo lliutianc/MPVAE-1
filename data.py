430f8ad82ebacd211517d6e4f54bc162c3e3dabe
lliutianc <lliutianc@gmail.com>
2021-10-07 01:02:33 -0400

    make all labels in adult binary

--- data.py
+++ data.py
@@ -33,6 +33,9 @@ def load_adult(subset):
     feat['capital_loss'] = (feat['capital_loss'] - feat['capital_loss'].min()) / \
                            (feat['capital_loss'].max() - feat['capital_loss'].min())
 
+    labels['occupation'] = labels['occupation'].apply(lambda x: x in [' Handlers-cleaners',' Craft-repair', ' Transport-moving', ' Farming-fishing']).astype(int)
+    labels['workclass'] = labels['occupation'].apply(lambda x: x in [' State-gov', ' Federal-gov', ' Local-gov']).astype(int)
+
     sensitive = ['race', 'sex']
     return feat, labels, sensitive
 
