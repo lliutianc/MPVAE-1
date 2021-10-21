4f72cadfe461c84585cf8101d02764ac0539ce3f
lliutianc <lliutianc@gmail.com>
2021-10-20 22:54:31 -0400

    test slurm config

--- run_fairreg.sh
+++ run_fairreg.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+# FILENAME: run_fairreg.sh
+#SBATCH -A partner
+
+#SBATCH --nodes=1 --time=01:00:00
+
 module load anaconda
 conda activate fairmlc
 
+module load cuda
+module load cudnn
+module load ml-toolkit-gpu/pytorch/1.7.1
+
+echo $CUDA_VISIBLE_DEVICES
+
+
 python fairreg.py -dataset adult
+
+
+#cd /home/liu3351/NC/
+
+#module load learning/conda-2020.11-py38-gpu
+#python -X faulthandler mbert_fed_kmeans_all_layer.py
