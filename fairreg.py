9f6ace336b44a87cdd127f5d89f20058fc62716d
lliutianc <lliutianc@gmail.com>
2021-10-20 23:14:34 -0400

    use cpu to debug

--- fairreg.py
+++ fairreg.py
@@ -17,7 +17,8 @@ from model import VAE, compute_loss
 from fairmodel import FairCritic, compute_fair_loss
 from data import load_data
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device('cpu')
 sys.path.append('./')
 THRESHOLDS = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.15, 0.20, 0.25, 0.30,
               0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.8, 0.85, 0.9, 0.95]
