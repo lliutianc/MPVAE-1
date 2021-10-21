26f184a3c10ad9cbb50b22be43d1d6a1723bd914
lliutianc <lliutianc@gmail.com>
2021-10-21 03:30:17 -0400

    debug dev

--- fairreg.py
+++ fairreg.py
@@ -231,13 +231,12 @@ def regularzie_mpvae_unfair(data, model, optimizer, args, use_valid=True):
     labels_z_unfair = 0.
     feats_z_unfair = 0.
     label_centroids = torch.unique(clusters)
-    sensitive_centroids = torch.unique(sensitive_feat, axis=0)
+    sensitive_centroids = torch.unique(sensitive_feat, dim=0)
 
     label_centroids = torch.from_numpy(label_centroids).to(device)
     sensitive_centroids = torch.from_numpy(sensitive_centroids).to(device)
 
     idx = torch.arange(clusters.shape[0])
-    print(idx)
     for centroid in label_centroids:
         cluster_labels_z = labels_z[idx[torch.equal(clusters, centroid)]]
         print(idx[torch.equal(clusters, centroid)], idx[torch.equal(clusters, centroid)].shape)
@@ -245,8 +244,8 @@ def regularzie_mpvae_unfair(data, model, optimizer, args, use_valid=True):
         if len(cluster_labels_z):
             for sensitive in sensitive_centroids:
                 sensitive_centroid = torch.all([
-                    torch.all(torch.equal(sensitive_centroids, sensitive), axis=1),  # sensitive level
-                    torch.equal(clusters, centroid)], axis=1)
+                    torch.all(torch.equal(sensitive_centroids, sensitive), dim=1),  # sensitive level
+                    torch.equal(clusters, centroid)], dim=1)
                 cluster_labels_z_sensitive = labels_z[idx[sensitive_centroid]]
                 print(len(cluster_labels_z_sensitive))
                 if len(cluster_labels_z_sensitive):
