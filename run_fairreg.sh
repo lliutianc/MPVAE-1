88995d6374f5dac6c76d2dc40250d35fac155d97
lliutianc <lliutianc@gmail.com>
2021-10-20 23:10:49 -0400

    test

--- run_fairreg.sh
+++ run_fairreg.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
 # FILENAME: run_fairreg.sh
-#SBATCH -A partner
 
-#SBATCH --nodes=1 --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:p100:1
+#SBATCH --time=01:00:00
+#SBATCH -A partner
+#SBATCH -J run_fairreg
+
 
 module load anaconda
 conda activate fairmlc
 
+module purge
 module load cuda
 module load cudnn
 module load ml-toolkit-gpu/pytorch/1.7.1
+module load learning/conda-2020.11-py38-gpu
+
+module list
 
 echo $CUDA_VISIBLE_DEVICES
-
 
 python fairreg.py -dataset adult
 
 
 #cd /home/liu3351/NC/
 
-#module load learning/conda-2020.11-py38-gpu
 #python -X faulthandler mbert_fed_kmeans_all_layer.py
