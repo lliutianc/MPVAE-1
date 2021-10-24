ff50d99b427921905eb203682697ad4efecf0e02
lliutianc <lliutianc@gmail.com>
2021-10-24 16:04:52 -0400

    use k80

--- run_baseline.sh
+++ run_baseline.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --job-name=fairmlc-regularized
-#SBATCH --gres=gpu:v100:1
+#SBATCH --gres=gpu:k80:1
 #SBATCH --mem-per-cpu=16G
 #SBATCH --mail-type=END
 
-#SBATCH --time=5:00:00
+#SBATCH --time=8:00:00
 #SBATCH --output=sbatch-logs/%x-%j.SLURM
 
 module load Conda/3
