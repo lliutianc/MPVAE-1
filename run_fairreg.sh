8f6cbfd06e109facbfa4bf9dff52feb50a638624
lliutianc <lliutianc@gmail.com>
2021-10-24 02:06:03 -0400

    run two priors

--- run_fairreg.sh
+++ run_fairreg.sh
@@ -5,13 +5,12 @@
 #SBATCH --mem-per-cpu=16G
 #SBATCH --mail-type=END
 
-#SBATCH --time=5:00:00
+#SBATCH --time=10:00:00
 #SBATCH --output=sbatch-logs/%x-%j.SLURM
 
 module load Conda/3
 conda activate fairmlc
 
-#python fairreg.py -dataset adult -latent_dim 8 -resume -labels_cluster_distance_threshold 0.01 -epoch 50 -labels_embed_method mpvae
-
+python fairreg.py -dataset adult -latent_dim 8 -resume -labels_cluster_distance_threshold 0.01 -epoch 50 -labels_embed_method mpvae
 python fairreg.py -dataset adult -latent_dim 8 -resume -labels_cluster_distance_threshold 0.01 -epoch 50 -labels_embed_method cbow
 
