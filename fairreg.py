5655195de7e1be380eded01ee2db34a712c91389
lliutianc <lliutianc@gmail.com>
2021-10-24 18:00:45 -0400

    detect device from model

--- fairreg.py
+++ fairreg.py
@@ -316,10 +316,11 @@ def train_mpvae_one_epoch(
                 c_loss * args.c_coeff, c_loss_x * args.c_coeff, kl_loss, total_loss))
 
         current_loss, val_metrics = validate_mpvae(
-            model, data.input_feat, data.labels, data.valid_idx, args)
+            model, data.input_feat, data.labels, data.valid_idx)
 
 
-def validate_mpvae(model, feat, labels, valid_idx, args):
+def validate_mpvae(model, feat, labels, valid_idx):
+    device = next(model.parameters().device)
     with torch.no_grad():
         model.eval()
         print("performing validation...")
