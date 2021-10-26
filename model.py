31d2b01955424178173a776d224310b06ddea5fe
lliutianc <lliutianc@gmail.com>
2021-10-26 17:29:42 -0400

    debug

--- model.py
+++ model.py
@@ -157,6 +157,7 @@ def compute_loss(input_label, fe_out, fe_mu, fe_logvar, fx_out, fx_mu, fx_logvar
     # see equation (3) in the paper for this block
     B = r_sqrt_sigma.T.float().to(device)
     print(B)
+    print('fe_out:', fe_out.min(), fe_out.max())
     sample_r = torch.tensordot(noise, B, dims=1) + fe_out #tensor: n_sample*n_batch*label_dim
     sample_r_x = torch.tensordot(noise, B, dims=1) + fx_out #tensor: n_sample*n_batch*label_dim
     norm = torch.distributions.normal.Normal(
