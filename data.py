51492278485a63c7f66af8d3d2e7b9c449168c97
lliutianc <lliutianc@gmail.com>
2021-11-09 00:25:33 -0500

    logic updates in preprocess and load
    use onehot or cate encode, decouple from load_data, remove fnlwgt feat in adult

--- data.py
+++ data.py
@@ -10,9 +10,9 @@ DATASETPATH = 'dataset/'
 
 def load_adult(subset):
     _header = [
-        'age', 'workclass', 'fnlwgt', 'education', 'education_num', 
-        'marital_stat', 'occupation', 'relationship', 'race', 'sex', 
-        'capital_gain', 'capital_loss', 'hours_per_week', 'native_country', 
+        'age', 'workclass', 'fnlwgt', 'education', 'education_num',
+        'marital_stat', 'occupation', 'relationship', 'race', 'sex',
+        'capital_gain', 'capital_loss', 'hours_per_week', 'native_country',
         'income_level'
     ]
     if subset != 'test':
@@ -22,16 +22,19 @@ def load_adult(subset):
     df = pd.read_csv(dfpath, header=None)
     df.columns = _header
     df['income_level'] = (df['income_level'] == ' <=50K')
-    
+
     label_cols = ['income_level', 'occupation', 'workclass']
     labels = df[label_cols]
     feat = df.drop(label_cols, axis=1)
-
-    feat['fnlwgt'] = feat['fnlwgt'] / feat['fnlwgt'].sum()
+    # The exact computation of fnlwgt is unclear, while it is likely to be highly correlated to sensitive features
+    feat = feat.drop('fnlwgt', axis=1)
+    # feat['fnlwgt'] = feat['fnlwgt'] / feat['fnlwgt'].sum()
     feat['capital_gain'] = (feat['capital_gain'] - feat['capital_gain'].min()) / \
-                           (feat['capital_gain'].max() - feat['capital_gain'].min())
+                           (feat['capital_gain'].max() -
+                            feat['capital_gain'].min())
     feat['capital_loss'] = (feat['capital_loss'] - feat['capital_loss'].min()) / \
-                           (feat['capital_loss'].max() - feat['capital_loss'].min())
+                           (feat['capital_loss'].max() -
+                            feat['capital_loss'].min())
 
     # labels['occupation'] = labels['occupation'].apply(lambda x: x in [' Handlers-cleaners',' Craft-repair', ' Transport-moving', ' Farming-fishing']).astype(int)
     # labels['workclass'] = labels['workclass'].apply(lambda x: x in [' Self-emp-not-inc', ' Private', ' Self-emp-inc']).astype(int)
@@ -64,7 +67,7 @@ def _load_donor_label(subset=None):
         'three_or_more_non_teacher_referred_donors',
         'one_non_teacher_referred_donor_giving_100_plus',
         'donation_from_thoughtful_donor'
-        ]:
+    ]:
         labels[binary_feat] = (labels[binary_feat] == 't')
 
     return labels
@@ -79,13 +82,15 @@ def _load_donor_resource_cost(subset=None):
     if subset:
         resource = pd.merge(resource, subset, how='inner', on='projectid')
     resource = resource[0 <= resource['item_unit_price'] <= 1e-5]
-    resource['item_cost'] = resource['item_unit_price'] * resource['item_quantity']
-    resource = resource.groupby('projectid').agg({'item_cost': np.sum}).reset_index()
+    resource['item_cost'] = resource['item_unit_price'] * \
+        resource['item_quantity']
+    resource = resource.groupby('projectid').agg(
+        {'item_cost': np.sum}).reset_index()
     resource = resource.rename(columns={'item_cost': 'resource_cost'})
 
     return resource
 
-    
+
 def _load_donor_projects(subset=None):
     feat_cols = [
         'projectid',
@@ -114,7 +119,7 @@ def _load_donor_projects(subset=None):
         'eligible_double_your_impact_match',
         'eligible_almost_home_match'
     ]
-    
+
     feat = pd.read_csv(DATASETPATH + 'donor/projects.csv')[feat_cols]
     if subset:
         feat = pd.merge(feat, subset, how='inner', on='projectid')
@@ -130,19 +135,19 @@ def _load_donor_projects(subset=None):
         'teacher_ny_teaching_fellow',
         'eligible_double_your_impact_match',
         'eligible_almost_home_match',
-        ]:
+    ]:
         feat[binary_feat] = (feat[binary_feat] == 't')
 
     sensitive = ['poverty_level', 'teacher_prefix']
     return feat, sensitive
-    
-    
+
+
 def load_donor(subset):
-    if subset == 'test': 
+    if subset == 'test':
         subset = _load_test_projectid()
     else:
         subset = None
-    
+
     labels = _load_donor_label(subset)
     feat, sensitive = _load_donor_projects(subset)
     # resource_cost  = _load_donor_resource_cost(subset)
@@ -157,69 +162,91 @@ def load_donor(subset):
         'total_price_excluding_optional_support',
         'total_price_including_optional_support',
         'students_reached'
-        ]:
+    ]:
         feat[normalize] = (feat[normalize] - feat[normalize].min()) / \
                           (feat[normalize].max() - feat[normalize].min())
 
     return feat, labels, sensitive
 
 
-def cast_to_numpy(df, use_onehot=True):
-    npy = np.array(df)
-    if use_onehot:
-        npy_oh = []
-        for i in range(npy.shape[1]):
-            col = npy[:, i]
-            if isinstance(col[0], str):
-                col = onehot(col)
-            if len(col.shape) == 1:
-                col = col[:, np.newaxis]
-            npy_oh.append(col)
-        npy = np.concatenate(npy_oh, axis=1)
+def preprocess(df, categorical_encode, show_categorcial_idx=False):
+    df = np.array(df)
+    if categorical_encode not in ['onehot', 'categorical', None]:
+        raise ValueError('Unrecognized categorical_encode')
 
-    return npy.astype(np.float32)
+    npy_cast = []
+    npy_str_idx = []
+    for i in range(df.shape[1]):
+        col = df[:, i]
+        if isinstance(col[0], str):
+            if categorical_encode:
+                npy_str_idx.append(i)
+                if categorical_encode == 'onehot':
+                    col = onehot(col)
+                else:
+                    col = categroical(col)
+        else:
+            col = col.astype(np.float32)
 
+        if len(col.shape) == 1:
+            col = col[:, np.newaxis]
+        npy_cast.append(col)
+    npy = np.concatenate(npy_cast, axis=1)
 
+    if not show_categorcial_idx:
+        return npy
+    else:
+        return npy, npy_str_idx
+
+    
 def onehot(col):
+    col = col.astype(str)
     unique_val = np.array(list(set(col)))
-    return np.equal(unique_val[np.newaxis, :], col[:, np.newaxis]).astype(np.int32)
+    return np.char.equal(unique_val[np.newaxis, :], col[:, np.newaxis]).astype(np.int32)
 
 
-def load_data(dataset, mode, separate_sensitive=False, use_onehot=True):
+def categroical(col):
+    return np.argmax(onehot(col), 1).astype(np.int32)
+
+
+def load_data(dataset, mode, separate_sensitive=False, categorical_encode='onehot'):
+    if categorical_encode not in ['onehot', 'categorical', None]:
+        raise ValueError('Unrecognized categorical_encode')
     if dataset not in ['adult', 'donor']:
         raise NotImplementedError()
 
     datapath = DATASETPATH + dataset
-    if use_onehot:
-        sensitive_featfile = os.path.join(datapath, 'sensitive_oh.npy')
-        nonsensitive_featfile = os.path.join(datapath, 'nonsensitive_oh.npy')
-        labelfile = os.path.join(datapath, 'label_oh.npy')
-    else:
-        sensitive_featfile = os.path.join(datapath, 'sensitive.npy')
-        nonsensitive_featfile = os.path.join(datapath, 'nonsensitive.npy')
-        labelfile = os.path.join(datapath, 'label.npy')
-
+    sensitive_featfile = os.path.join(
+        datapath, f'sensitive_{categorical_encode}.npy')
+    nonsensitive_featfile = os.path.join(
+        datapath, f'nonsensitive_{categorical_encode}.npy')
+    labelfile = os.path.join(
+        datapath, f'label_{categorical_encode}.npy')
+    
     if not allexists(sensitive_featfile, nonsensitive_featfile, labelfile):
-        print('prepare dataset...')
+        print(f'prepare dataset: {dataset}...')
         if dataset == 'adult':
             feat, labels, sensitive = load_adult(mode)
         elif dataset == 'donor':
             feat, labels, sensitive = load_donor(mode)
         else:
-            raise ValueError()
+            raise NotImplementedError()
 
         sensitive_feat = feat[sensitive]
         nonsensitive_feat = feat.drop(sensitive, axis=1)
 
-        nonsensitive_feat = cast_to_numpy(nonsensitive_feat, use_onehot)
-        sensitive_feat = cast_to_numpy(sensitive_feat, use_onehot)
-        labels = cast_to_numpy(labels, use_onehot)
+        nonsensitive_feat = preprocess(
+            nonsensitive_feat, categorical_encode)
+        sensitive_feat = preprocess(
+            sensitive_feat, categorical_encode)
+        labels = preprocess(
+            labels, categorical_encode)
 
         np.save(open(sensitive_featfile, 'wb'), sensitive_feat)
         np.save(open(nonsensitive_featfile, 'wb'), nonsensitive_feat)
         np.save(open(labelfile, 'wb'), labels)
     else:
-        print('load existing datasets...')
+        print('load existing dataset: {dataset}...')
 
         sensitive_feat = np.load(open(sensitive_featfile, 'rb'))
         nonsensitive_feat = np.load(open(nonsensitive_featfile, 'rb'))
@@ -229,5 +256,3 @@ def load_data(dataset, mode, separate_sensitive=False, use_onehot=True):
         return nonsensitive_feat, sensitive_feat, labels
     else:
         return np.concatenate([nonsensitive_feat, sensitive_feat], axis=1), labels
-
-
