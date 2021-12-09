1745ea4a30c67b1d57cbbc1865d08204f54c319c
lliutianc <lliutianc@gmail.com>
2021-12-08 20:01:39 -0500

    debug

--- fairsoft_arule.py
+++ fairsoft_arule.py
@@ -42,7 +42,7 @@ def train_mpvae_softfair_one_epoch(
         target_fair_label = ''.join(target_fair_label.astype(str))
         target_fair_labels_str.append(target_fair_label)
     target_fair_labels = target_fair_labels_str
-    # print(target_fair_labels)
+    print(target_fair_labels)
 
     np.random.shuffle(data.train_idx)
     args.device = next(model.parameters()).device
@@ -345,7 +345,7 @@ def train_fair_through_regularize():
     label_type, count = np.unique(labels, axis=0, return_counts=True)
     count_sort_idx = np.argsort(-count)
     label_type = label_type[count_sort_idx]
-    target_fair_labels = label_type[:1].astype(int)
+    target_fair_labels = [label_type[:1].astype(int)]
     # print(target_fair_labels, count[count_sort_idx])
 
     print('start training fair mpvae...')
--- fairsoft_baseline.py
+++ fairsoft_baseline.py
@@ -340,7 +340,7 @@ def train_fair_through_regularize():
     label_type, count = np.unique(labels, axis=0, return_counts=True)
     count_sort_idx = np.argsort(-count)
     label_type = label_type[count_sort_idx]
-    target_fair_labels = label_type[:1].astype(int)
+    target_fair_labels = [label_type[:1].astype(int)]
     # print(target_fair_labels, count[count_sort_idx])
 
     print('start training fair mpvae...')
