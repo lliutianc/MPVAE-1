d4697f186ffd18841b2b8593359ebcbc82a53e05
lliutianc <lliutianc@gmail.com>
2021-12-08 20:03:41 -0500

    debug

--- fairsoft_baseline.py
+++ fairsoft_baseline.py
@@ -40,7 +40,9 @@ def train_mpvae_softfair_one_epoch(
         target_fair_label = ''.join(target_fair_label.astype(str))
         target_fair_labels_str.append(target_fair_label)
     target_fair_labels = target_fair_labels_str
-
+    print(target_fair_labels)
+    exit(1)
+    
     np.random.shuffle(data.train_idx)
     args.device = next(model.parameters()).device
 
