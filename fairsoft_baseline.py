33b3bb59e1ac323204b45aae0e3c982d49b9061f
lliutianc <lliutianc@gmail.com>
2021-12-22 20:53:12 -0500

    correct train_new logic, add dp baseline

--- fairsoft_baseline.py
+++ fairsoft_baseline.py
@@ -13,19 +13,91 @@ import pandas as pd
 from utils import build_path
 from mpvae import VAE
 from data import load_data
-from label_distance import indication_similarity
+from label_distance import indication_similarity, constant_similarity
 
 from main import THRESHOLDS, METRICS
 from fairsoft_train import train_mpvae_softfair_one_epoch
 
 
 def train_fair_through_regularize(args):
+    train_fair_equalodds_through_regularize(args)
+    train_fair_demparity_through_regularize(args)
 
+
+def train_fair_demparity_through_regularize(args):
+    hparams = f'constant_function'
+    label_dist_path = os.path.join(
+        args.model_dir, f'label_dist-{hparams}.npy')
+
+    if args.train_new == 0 and os.path.exists(label_dist_path):
+        label_dist = pickle.load(open(label_dist_path, 'rb'))
+    else:
+        label_dist = constant_similarity(args)
+        pickle.dump(label_dist, open(label_dist_path, 'wb'))
+
+    np.random.seed(4)
+    nonsensitive_feat, sensitive_feat, labels = load_data(
+        args.dataset, args.mode, True, 'onehot')
+
+    # Test fairness on some labels
+    label_type, count = np.unique(labels, axis=0, return_counts=True)
+    count_sort_idx = np.argsort(-count)
+    label_type = label_type[count_sort_idx]
+    idx = args.target_label_idx  # idx choices: 0, 10, 20, 50
+    target_fair_labels = label_type[idx: idx + 1].astype(int)
+
+    train_cnt, valid_cnt = int(
+        len(nonsensitive_feat) * 0.7), int(len(nonsensitive_feat) * .2)
+    train_idx = np.arange(train_cnt)
+    valid_idx = np.arange(train_cnt, valid_cnt + train_cnt)
+    one_epoch_iter = np.ceil(len(train_idx) / args.batch_size)
+
+    data = types.SimpleNamespace(
+        input_feat=nonsensitive_feat, labels=labels, sensitive_feat=sensitive_feat,
+        train_idx=train_idx, valid_idx=valid_idx, batch_size=args.batch_size)
+    args.feature_dim = data.input_feat.shape[1]
+    args.label_dim = data.labels.shape[1]
+
+    fair_vae = VAE(args).to(args.device)
+    fair_vae.train()
+
+    if args.penalize_unfair:
+        fair_vae_checkpoint_path = os.path.join(
+            args.model_dir, f'fair_vae_prior-{hparams}.pkl')
+    else:
+        fair_vae_checkpoint_path = os.path.join(
+            args.model_dir, 'unfair_vae_prior.pkl')
+    
+    if args.train_new == 0 and os.path.exists(fair_vae_checkpoint_path):
+        print('find trained mpvae...')
+    else:
+        print('train a new mpvae...')
+
+        optimizer = optim.Adam(fair_vae.parameters(),
+                            lr=args.learning_rate, weight_decay=1e-5)
+
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer, one_epoch_iter * (args.max_epoch / args.lr_decay_times), args.lr_decay_ratio)
+
+        print('start training fair mpvae...')
+        for _ in range(args.max_epoch):
+            train_mpvae_softfair_one_epoch(
+                data, fair_vae, optimizer, scheduler,
+                penalize_unfair=args.penalize_unfair,
+                target_fair_labels=target_fair_labels,
+                label_distances=label_dist,
+                eval_after_one_epoch=True,
+                args=args)
+
+        torch.save(fair_vae.cpu().state_dict(), fair_vae_checkpoint_path)
+
+
+def train_fair_equalodds_through_regularize(args):
     hparams = f'indication_function'
     label_dist_path = os.path.join(
         args.model_dir, f'label_dist-{hparams}.npy')
 
-    if args.resume and os.path.exists(label_dist_path):
+    if args.train_new == 0 and os.path.exists(label_dist_path):
         label_dist = pickle.load(open(label_dist_path, 'rb'))
     else:
         label_dist = indication_similarity(args)
@@ -64,29 +136,29 @@ def train_fair_through_regularize(args):
     else:
         fair_vae_checkpoint_path = os.path.join(
             args.model_dir, 'unfair_vae_prior.pkl')
-    if args.resume and os.path.exists(fair_vae_checkpoint_path):
-        print('use a trained fair mpvae...')
-        fair_vae.load_state_dict(torch.load(fair_vae_checkpoint_path))
+
+    if args.train_new == 0 and os.path.exists(fair_vae_checkpoint_path):
+        print('find trained mpvae...')
     else:
-        print('train a new fair mpvae...')
+        print('train a new mpvae...')
 
-    optimizer = optim.Adam(fair_vae.parameters(),
-                           lr=args.learning_rate, weight_decay=1e-5)
+        optimizer = optim.Adam(fair_vae.parameters(),
+                            lr=args.learning_rate, weight_decay=1e-5)
 
-    scheduler = torch.optim.lr_scheduler.StepLR(
-        optimizer, one_epoch_iter * (args.max_epoch / args.lr_decay_times), args.lr_decay_ratio)
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer, one_epoch_iter * (args.max_epoch / args.lr_decay_times), args.lr_decay_ratio)
 
-    print('start training fair mpvae...')
-    for _ in range(args.max_epoch):
-        train_mpvae_softfair_one_epoch(
-            data, fair_vae, optimizer, scheduler,
-            penalize_unfair=args.penalize_unfair, 
-            target_fair_labels=target_fair_labels, 
-            label_distances=label_dist,
-            eval_after_one_epoch=True, 
-            args=args)
+        print('start training fair mpvae...')
+        for _ in range(args.max_epoch):
+            train_mpvae_softfair_one_epoch(
+                data, fair_vae, optimizer, scheduler,
+                penalize_unfair=args.penalize_unfair,
+                target_fair_labels=target_fair_labels,
+                label_distances=label_dist,
+                eval_after_one_epoch=True,
+                args=args)
 
-    torch.save(fair_vae.cpu().state_dict(), fair_vae_checkpoint_path)
+        torch.save(fair_vae.cpu().state_dict(), fair_vae_checkpoint_path)
 
 
 if __name__ == '__main__':
--- label_distance.py
+++ label_distance.py
@@ -126,6 +126,46 @@ def indication_similarity(args):
     return dist_dict
 
 
+def constant_similarity(args):
+    np.random.seed(4)
+    _, _, labels = load_data(
+        args.dataset, args.mode, True, None)
+    labels_oh = preprocess(labels, 'onehot').astype(int)
+    labels = labels.astype(str)
+
+    labels_oh_str = np.concatenate([labels_oh.astype(str), labels], axis=1)
+    labels_oh_str = np.unique(labels_oh_str, axis=0)
+
+    labels_express = {}
+    for label in labels_oh_str:
+        label_oh = label[:-3]
+        label_str = label[-3:]
+        labels_express[frozenset(label_str)] = label_oh.astype(int)
+
+    income_level = np.unique(labels[:, 0])
+    occupation = np.unique(labels[:, 1])
+    workclass = np.unique(labels[:, 2])
+    labelsets = []
+    for income in income_level:
+        for occu in occupation:
+            for work in workclass:
+                labelsets.append(set([income, occu, work]))
+
+    dist_dict = {}
+    for p1 in labelsets:
+        p1_oh = labels_express.get(frozenset(p1), None)
+        if p1_oh is not None:
+            lab1 = ''.join(p1_oh.astype(str))
+            dist_dict[lab1] = {}
+            for p2 in labelsets:
+                p2_oh = labels_express.get(frozenset(p2), None)
+                if p2_oh is not None:
+                    lab2 = ''.join(p2_oh.astype(str))
+                    dist_dict[lab1][lab2] = 1.0
+
+    return dist_dict
+
+
 def str_ham_similarity(str1, str2):
     if len(str1) != len(str2):
         raise ValueError('Incompatible string pairs: have different lengths.')
