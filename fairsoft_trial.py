dfac0ca5cbb8851eb2e4502529558e80e7398103
lliutianc <lliutianc@gmail.com>
2022-01-05 16:14:15 -0500

    add specified target_label_idx case

--- fairsoft_trial.py
+++ fairsoft_trial.py
@@ -128,7 +128,7 @@ if __name__ == '__main__':
         train_fairsoft_hamming(args)
         train_fairsoft_baseline(args)
         eval_fairsoft_allmodels(args)
-    else:
+    elif args.target_label_idx is None:
         # train unfair model
         for target_label_idx in [0, 10, 20, 50]:
             args.target_label_idx = target_label_idx
@@ -146,8 +146,22 @@ if __name__ == '__main__':
             train_fairsoft_baseline(args)
             eval_fairsoft_allmodels(args)
 
-            # TODO: remove this break after developement!
+            # TODO: remove this break after developement
             break
+    else:
+        args.penalize_unfair = 0
+        train_fairsoft_baseline(args)
+
+        args.penalize_unfair = 1
+        # for dist_gamma in [.1, .5, 1., 1.5, 2.]:
+        for dist_gamma in [1.]:
+            args.dist_gamma = dist_gamma
+            train_fairsoft_arule(args)
+
+        train_fairsoft_hamming(args)
+        train_fairsoft_baseline(args)
+        eval_fairsoft_allmodels(args)
+
 
 
 # python fairsoft_trial.py -dataset adult -latent_dim 8 -cuda 5
