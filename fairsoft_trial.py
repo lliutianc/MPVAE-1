b63f6a58850f7526feb9f762be9654cd80dd7504
lliutianc <lliutianc@gmail.com>
2022-01-09 19:37:04 -0500

    correct log name

--- fairsoft_trial.py
+++ fairsoft_trial.py
@@ -57,7 +57,7 @@ def eval_fairsoft_allmodels(args):
     args.model_dir = f'fair_through_distance/model/{args.dataset}'
     if args.mask_target_label:
         logger = Logger(os.path.join(
-            args.model_dir, f'evalution-{args.target_label_idx}masked.txt'))
+            args.model_dir, f'evalution-{args.target_label_idx}_masked.txt'))
     else:
         logger = Logger(os.path.join(
             args.model_dir, f'evalution-{args.target_label_idx}.txt'))
@@ -126,23 +126,23 @@ if __name__ == '__main__':
         # for dist_gamma in [.1, .5, 1., 1.5, 2.]:
         for dist_gamma in [1.]:
             args.dist_gamma = dist_gamma
-            train_fairsoft_arule(args)
+            train_fairsoft_arule(args)  # 20308
 
-        train_fairsoft_hamming(args)
+        train_fairsoft_hamming(args)  # 21587
         train_fairsoft_baseline(args)
         eval_fairsoft_allmodels(args)
     elif args.target_label_idx is not None:
         args.penalize_unfair = 0
-        train_fairsoft_baseline(args)
+        train_fairsoft_baseline(args) # unfair model
 
         args.penalize_unfair = 1
         # for dist_gamma in [.1, .5, 1., 1.5, 2.]:
         for dist_gamma in [1.]:
                 args.dist_gamma = dist_gamma
-                train_fairsoft_arule(args)
+                train_fairsoft_arule(args)  # 20308 samples
 
-        train_fairsoft_hamming(args)
-        train_fairsoft_baseline(args)
+        train_fairsoft_hamming(args)  # 21587 samples
+        train_fairsoft_baseline(args)  # eo: 641 samples, dp: 21587 samples
         eval_fairsoft_allmodels(args)
     else:
         for target_label_idx in [0, 10, 20, 50]:
@@ -157,14 +157,12 @@ if __name__ == '__main__':
                 args.dist_gamma = dist_gamma
                 train_fairsoft_arule(args)
 
-            train_fairsoft_hamming(args)
-            train_fairsoft_baseline(args)
+            train_fairsoft_hamming(args) 
+            train_fairsoft_baseline(args)  
             eval_fairsoft_allmodels(args)
 
             # TODO: remove this break after developement
             break
         
 
-
-
-# python fairsoft_trial.py -dataset adult -latent_dim 8 -cuda 5
+# python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 1 -cuda 5
