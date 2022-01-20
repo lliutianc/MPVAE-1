7e0feb3214707155b344f4808fa48ba159f8fac1
lliutianc <lliutianc@gmail.com>
2022-01-20 17:34:47 -0500

    correct met name

--- fairsoft_trial.py
+++ fairsoft_trial.py
@@ -92,7 +92,7 @@ def eval_fairsoft_allmodels(args):
 
     print(fair_metrics_nested)
     # for met in ['constant_function', 'jaccard', 'hamming', 'arule', 'indication_function']:
-    for met in ['constant_function', 'jaccard', 'indication_function']:
+    for met in ['constant_function', 'jaccard_distance', 'indication_function']:
         if met in fair_metrics_nested:
             if len(fair_metrics_nested[met]) > 1:
                 met_sorted = sorted(
