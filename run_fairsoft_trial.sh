9c0f963a2ed81c0dbf81edddd5529988d56da1a0
lliutianc <lliutianc@gmail.com>
2022-01-20 18:37:51 -0500

    merge into old script

--- run_fairsoft_trial.sh
+++ run_fairsoft_trial.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name=fairmlc-soft-trial
+#SBATCH --job-name=run_fairsoft_trial
 #SBATCH --gres=gpu:k80:1
-#SBATCH --mem-per-cpu=16G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --mail-type=END
 
-#SBATCH --time=8:00:00
+#SBATCH --time=10:00:00
 #SBATCH --output=sbatch-logs/%x-%j.SLURM
+#SBATCH --array=0-4
 
 module load Conda/3
 conda activate fairmlc
 
-python fairsoft_trial.py -dataset adult -latent_dim 8 -train_new 0 -target_label 0000000000000010000001000
-python fairsoft_trial.py -dataset adult -latent_dim 8 -train_new 0
-
-
+python fairsoft_trial.py -dataset donor -latent_dim 8 -target_label_idx 0 -mask_target_label 0
