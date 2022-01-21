c8577d096f650efbc6408856da69dfca1fdb5efa
lliutianc <lliutianc@gmail.com>
2022-01-20 22:13:53 -0500

    unfair model path

--- fairsoft_baseline.py
+++ fairsoft_baseline.py
@@ -66,7 +66,7 @@ def train_fair_demparity_through_regularize(args):
             args.model_dir, f'fair_vae_prior-{hparams}-{args.seed:04d}.pkl')
     else:
         fair_vae_checkpoint_path = os.path.join(
-            args.model_dir, 'unfair_vae_prior-{args.seed:04d}.pkl')
+            args.model_dir, f'unfair_vae_prior-{args.seed:04d}.pkl')
 
     if args.train_new == 0 and os.path.exists(fair_vae_checkpoint_path):
         print(f'find trained mpvae: {fair_vae_checkpoint_path}...')
