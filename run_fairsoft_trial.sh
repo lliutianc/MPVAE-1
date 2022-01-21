dcfee1a057cf1059c13872aba484d81c4fb7ddab
lliutianc <lliutianc@gmail.com>
2022-01-21 09:09:05 -0500

    run donor

--- run_fairsoft_trial.sh
+++ run_fairsoft_trial.sh
@@ -7,13 +7,14 @@
 
 #SBATCH --time=10:00:00
 #SBATCH --output=sbatch-logs/%x-%j.SLURM
-#SBATCH --array=1-10
+#SBATCH --array=1
 
 module load Conda/3
 conda activate fairmlc
 
 
-python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0
-python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 1
+# python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0
+# python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 1
 
-# python fairsoft_trial.py -dataset donor -latent_dim 8 -target_label_idx 0 -mask_target_label 0
+python fairsoft_trial.py -dataset donor -latent_dim 8 -target_label_idx 0 -mask_target_label 0
+python fairsoft_trial.py -dataset donor -latent_dim 8 -target_label_idx 0 -mask_target_label 1
