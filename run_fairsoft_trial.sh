b82bf08cc4d283cf5e121a6b153348793cb72694
lliutianc <lliutianc@gmail.com>
2022-01-21 22:46:33 -0500

    run donor data

--- run_fairsoft_trial.sh
+++ run_fairsoft_trial.sh
@@ -5,16 +5,16 @@
 #SBATCH --mem-per-cpu=8G
 #SBATCH --mail-type=END
 
-#SBATCH --time=10:00:00
+#SBATCH --time=20:00:00
 #SBATCH --output=sbatch-logs/%x-%j.SLURM
-#SBATCH --array=1-10
+#SBATCH --array=1
 
 module load Conda/3
 conda activate fairmlc
 
 
-python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID
-python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 1 -seed $SLURM_ARRAY_TASK_ID
+# python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID
+# python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 1 -seed $SLURM_ARRAY_TASK_ID
 
-# python fairsoft_trial.py -dataset donor -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID
-# python fairsoft_trial.py -dataset donor -latent_dim 8 -target_label_idx 0 -mask_target_label 1 -seed $SLURM_ARRAY_TASK_ID
+python fairsoft_trial.py -dataset donor -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID
+python fairsoft_trial.py -dataset donor -latent_dim 8 -target_label_idx 0 -mask_target_label 1 -seed $SLURM_ARRAY_TASK_ID
