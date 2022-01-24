af18b77eb8c6209c50d82036d381bb8f3ec2f6ad
lliutianc <lliutianc@gmail.com>
2022-01-24 02:20:09 -0500

    larger reg

--- run_fairsoft_trial.sh
+++ run_fairsoft_trial.sh
@@ -17,11 +17,11 @@ conda activate fairmlc
 # python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 1 -seed $SLURM_ARRAY_TASK_ID
 
 
-python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -label_z_fair_coeff 100.
-python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 1 -seed $SLURM_ARRAY_TASK_ID -label_z_fair_coeff 100.
+python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -label_z_fair_coeff 500.
+python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 1 -seed $SLURM_ARRAY_TASK_ID -label_z_fair_coeff 500.
 
-python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -label_z_fair_coeff 100.
-python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 1 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -label_z_fair_coeff 100.
+python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -label_z_fair_coeff 500.
+python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 1 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -label_z_fair_coeff 500.
 
 
 # python fairsoft_trial.py -dataset donor -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -label_z_fair_coeff 5. -feat_z_fair_coeff 5.
