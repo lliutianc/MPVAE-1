cad6d82c6735612e8d2627ed217eaafacd8fd1f3
lliutianc <lliutianc@gmail.com>
2022-01-25 14:05:51 -0500

    correct param name

--- fairsoft_evaluate.py
+++ fairsoft_evaluate.py
@@ -338,7 +338,7 @@ def evaluate_over_labels(target_fair_labels, args, logger=Logger()):
             if args.mask_target_label:
                 model_prior += '_masked'
         model_files = search_files(os.path.join(
-            args.model_dir,  model_prior), postfix=f'-{args.label_z_fair_coeff:.2f}_{args.seed:04d}.pkl')
+            args.model_dir,  model_prior), postfix=f'-{args.fair_coeff:.2f}_{args.seed:04d}.pkl')
         if len(model_files):
             model_paths += [os.path.join(
                 args.model_dir, model_prior, model_file) for
