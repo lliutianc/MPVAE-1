ed27f9775822fb6f0a08e5fe4b61a67e5c2abd55
lliutianc <lliutianc@gmail.com>
2022-01-26 21:20:43 -0500

    download both in process and post process results

--- download_replication_result.sh
+++ download_replication_result.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-rsync -av --progress liutia25@hpcc.msu.edu:dev/fairness/mpvae/fair_through_distance/model/credit/evaluation-0_masked evaluation-credit/
-rsync -av --progress liutia25@hpcc.msu.edu:dev/fairness/mpvae/fair_through_distance/model/credit/evaluation-0 evaluation-credit/
+rsync -av --progress liutia25@hpcc.msu.edu:dev/fairness/mpvae/fair_through_distance/model/credit/evaluation-0_masked inprocess/evaluation-credit/
+rsync -av --progress liutia25@hpcc.msu.edu:dev/fairness/mpvae/fair_through_distance/model/credit/evaluation-0 inprocess/evaluation-credit/
 
-rsync -av --progress liutia25@hpcc.msu.edu:dev/fairness/mpvae/fair_through_distance/model/adult/evaluation-0_masked evaluation-adult/
-rsync -av --progress liutia25@hpcc.msu.edu:dev/fairness/mpvae/fair_through_distance/model/adult/evaluation-0 evaluation-adult/
+rsync -av --progress liutia25@hpcc.msu.edu:dev/fairness/mpvae/fair_through_distance/model/adult/evaluation-0_masked inprocess/evaluation-adult/
+rsync -av --progress liutia25@hpcc.msu.edu:dev/fairness/mpvae/fair_through_distance/model/adult/evaluation-0 inprocess/evaluation-adult/
+
+
+
+
+rsync -av --progress liutia25@hpcc.msu.edu:dev/fairness/mpvae/fair_through_postprocess/model/credit/evaluation-0_masked postprocess/evaluation-credit/
+rsync -av --progress liutia25@hpcc.msu.edu:dev/fairness/mpvae/fair_through_postprocess/model/credit/evaluation-0 postprocess/evaluation-credit/
+
+rsync -av --progress liutia25@hpcc.msu.edu:dev/fairness/mpvae/fair_through_postprocess/model/adult/evaluation-0_masked postprocess/evaluation-adult/
+rsync -av --progress liutia25@hpcc.msu.edu:dev/fairness/mpvae/fair_through_postprocess/model/adult/evaluation-0 postprocess/evaluation-adult/
