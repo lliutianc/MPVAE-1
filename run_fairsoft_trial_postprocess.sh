4148a460cb1b74b4acb244b72e86727649d4cead
lliutianc <lliutianc@gmail.com>
2022-01-27 02:34:27 -0500

    run with old hparams

--- run_fairsoft_trial_postprocess.sh
+++ run_fairsoft_trial_postprocess.sh
@@ -13,17 +13,32 @@ module load Conda/3
 conda activate fairmlc
 
 
-python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -lr 1e-4 -fair_coeff 0.1 
-python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -lr 1e-4 -fair_coeff 0.5 
-python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -lr 1e-4 -fair_coeff 1
-python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -lr 1e-4 -fair_coeff 5 
-python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -lr 1e-4 -fair_coeff 10
+# python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -lr 1e-4 -fair_coeff 0.1 
+# python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -lr 1e-4 -fair_coeff 0.5 
+# python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -lr 1e-4 -fair_coeff 1
+# python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -lr 1e-4 -fair_coeff 5 
+# python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -lr 1e-4 -fair_coeff 10
 
-python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -lr 1e-4 -fair_coeff 0.1
-python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -lr 1e-4 -fair_coeff 0.5
-python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -lr 1e-4 -fair_coeff 1
-python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -lr 1e-4 -fair_coeff 5
-python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -lr 1e-4 -fair_coeff 10
+# python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -lr 1e-4 -fair_coeff 0.1
+# python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -lr 1e-4 -fair_coeff 0.5
+# python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -lr 1e-4 -fair_coeff 1
+# python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -lr 1e-4 -fair_coeff 5
+# python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -lr 1e-4 -fair_coeff 10
 
 
 
+python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -fair_coeff 0.1
+python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -fair_coeff 1
+python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -fair_coeff 10
+python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -fair_coeff 100
+python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -fair_coeff 500
+python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -fair_coeff 1000
+python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 200 -bs 32 -fair_coeff 5000
+
+python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -fair_coeff 0.1
+python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -fair_coeff 1
+python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -fair_coeff 10
+python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -fair_coeff 100
+python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -fair_coeff 500
+python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -fair_coeff 1000
+python fairsoft_trial_postprocess.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label 0 -seed $SLURM_ARRAY_TASK_ID -epoch 20 -bs 128 -fair_coeff 5000
