58000e4699e8c89d28837c7f72c4a9d563d3eab8
lliutianc <lliutianc@gmail.com>
2022-01-29 12:03:13 -0500

    parallel submission

--- python_runner.py
+++ python_runner.py
@@ -15,44 +15,61 @@ def generate_script_call_args(**kwargs):
     ] + list(itertools.chain(*[(f'-{k}', str(v)) for k, v in kwargs.items()]))
 
 
-def run_one_instance(seed=0, dataset='credit', batch_size=32, epochs=200, fair_coeff=0.1, debug=False):
-    args = generate_script_call_args(dataset=dataset,
-                                     latent_dim=8,
-                                     target_label_idx=0,
-                                     mask_target_label=1,
-                                     seed=seed,
-                                     epoch=epochs,
-                                     bs=batch_size,
-                                     fair_coeff=fair_coeff)
-    if debug:
-        kwargs = {
-            'capture_output': False,
-            'stderr': subprocess.STDOUT
-        }
-    else:
-        kwargs = {
-            'capture_output': False,
-            'stdout': subprocess.DEVNULL,
-            'stderr': subprocess.DEVNULL
-        }
-    subprocess.run(args, check=True, **kwargs)
+# def run_one_instance(seed=0, dataset='credit', batch_size=32, epochs=200, fair_coeff=0.1, debug=False):
+    
 
+def submit_one_instance(dataset='credit', mask_target_label=1, batch_size=32, epochs=200, fair_coeff=0.1, debug=False):
 
-def run_parallel_setting(dataset, batch_size, epochs, fair_coeff):
+    def run_one_instance(seed):
+        args = generate_script_call_args(dataset=dataset,
+                                         latent_dim=8,
+                                         target_label_idx=0,
+                                         mask_target_label=mask_target_label,
+                                         seed=seed,
+                                         epoch=epochs,
+                                         bs=batch_size,
+                                         fair_coeff=fair_coeff)
+        if debug:
+            kwargs = {
+                'capture_output': False,
+                'stderr': subprocess.STDOUT
+            }
+        else:
+            kwargs = {
+                'capture_output': False,
+                'stdout': subprocess.DEVNULL,
+                'stderr': subprocess.DEVNULL
+            }
+        subprocess.run(args, check=True, **kwargs)
+    
+    return run_one_instance
+    
+
+def run_parallel_setting(dataset, mask_target_label, batch_size, epochs, fair_coeff):
     # different seed, same fair coeff
     seeds = list(range(1, 11))
     Parallel(
         n_jobs=len(seeds),
         verbose=100,
     )(
-        delayed(run_one_instance(s, dataset, batch_size, epochs, fair_coeff)) for s in seeds
+        delayed(submit_one_instance(dataset, mask_target_label, batch_size, epochs, fair_coeff))(s) for s in seeds
     )
-
+ 
 
 if __name__ == '__main__':
 
     for fair_coeff in [0.1, 1., 10., 100., 500., 1000., 5000]:
-        run_parallel_setting('credit', 32, 200, fair_coeff)
-        run_parallel_setting('adult', 128, 20, fair_coeff)
+        run_parallel_setting(
+            dataset='credit', mask_target_label=1,  
+            batch_size=32, 
+            epochs=200, 
+            fair_coeff=fair_coeff
+            )
+        run_parallel_setting(
+            dataset='adult', mask_target_label=1,
+            batch_size=128,
+            epochs=20,
+            fair_coeff=fair_coeff
+        )
         
-# python fairsoft_trial_postprocess.py -dataset credit -latent_dim 8 - target_label_idx 0 - mask_target_label 0 - seed $SLURM_ARRAY_TASK_ID - epoch 200 - bs 32 - fair_coeff 0.1
+# python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 1 -seed 1 -epoch 200 -bs 32 -fair_coeff 0.1
