780c59f46a98807e46f8360b1a732b9b7b781899
lliutianc <lliutianc@gmail.com>
2022-01-29 23:51:53 -0500

    add cuda

--- simple_run_fairsoft.sh
+++ simple_run_fairsoft.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-conda activate fairmlc
-
 seed=$1
 mask_target_label=$2
+cuda=$3
 
-python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 0.1
-python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 1
-python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 10
-python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 100
-python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 500
-python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 1000
-python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 5000
+python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 0.1  -cuda $cuda
+python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 1 -cuda $cuda
+python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 10 -cuda $cuda
+python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 100 -cuda $cuda
+python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 500 -cuda $cuda
+python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 1000 -cuda $cuda
+python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 500 -bs 32 -fair_coeff 5000 -cuda $cuda
 
-python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 0.1
-python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 1
-python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 10
-python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 100
-python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 500
-python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 1000
-python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 5000
+python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 0.1 -cuda $cuda
+python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 1 -cuda $cuda
+python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 10 -cuda $cuda
+python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 100 -cuda $cuda
+python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 500 -cuda $cuda
+python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 1000 -cuda $cuda
+python fairsoft_trial.py -dataset adult -latent_dim 8 -target_label_idx 0 -mask_target_label $mask_target_label -seed $seed -epoch 20 -bs 128 -fair_coeff 5000 -cuda $cuda
 
 
 
