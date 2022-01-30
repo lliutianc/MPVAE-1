38aee14335383573610ca9514a29fffdda107ea2
lliutianc <lliutianc@gmail.com>
2022-01-29 22:38:05 -0500

    debug

--- python_runner.py
+++ python_runner.py
@@ -14,7 +14,6 @@ def generate_script_call_args(**kwargs):
         os.path.join(my_dir, 'fairsoft_trial.py')
     ] + list(itertools.chain(*[(f'-{k}', str(v)) for k, v in kwargs.items()]))
 
-    
 
 def submit_one_instance(dataset='credit', mask_target_label=1, batch_size=32, epochs=200, fair_coeff=0.1, debug=False):
     def run_one_instance(seed):
@@ -38,9 +37,9 @@ def submit_one_instance(dataset='credit', mask_target_label=1, batch_size=32, ep
                 'stderr': subprocess.DEVNULL
             }
         subprocess.run(args, check=True, **kwargs)
-    
+
     return run_one_instance
-    
+
 
 def run_parallel_setting(dataset, mask_target_label, batch_size, epochs, fair_coeff):
     # different seed, same fair coeff
@@ -51,15 +50,15 @@ def run_parallel_setting(dataset, mask_target_label, batch_size, epochs, fair_co
     )(
         delayed(submit_one_instance(dataset, mask_target_label, batch_size, epochs, fair_coeff))(s) for s in seeds
     )
- 
+
 
 if __name__ == '__main__':
 
     for fair_coeff in [0.1, 1., 10., 100., 500., 1000., 5000]:
         # run_parallel_setting(
-        #     dataset='credit', mask_target_label=1,  
-        #     batch_size=32, 
-        #     epochs=200, 
+        #     dataset='credit', mask_target_label=1,
+        #     batch_size=32,
+        #     epochs=200,
         #     fair_coeff=fair_coeff
         #     )
         # run_parallel_setting(
@@ -68,5 +67,6 @@ if __name__ == '__main__':
         #     epochs=20,
         #     fair_coeff=fair_coeff
         # )
-        submit_one_instance(dataset='adult', mask_target_label=1, batch_size=128, epoch=20, fair_coeff=1)
+        submit_one_instance(dataset='adult', mask_target_label=1,
+                            batch_size=128, epochs=20, fair_coeff=1)
 # python fairsoft_trial.py -dataset credit -latent_dim 8 -target_label_idx 0 -mask_target_label 1 -seed 1 -epoch 200 -bs 32 -fair_coeff 0.1
